26cca732ed97a37f0d9707d93f922552512a7474
DaveBrad <dbradley2@bell.net>
2017-12-16 15:01:20 -0500

    - basic test case creation for the various modes --- have a basic running interactive test, but not really interactive

--- test/func/HelpLicenseTest.java
+++ test/func/HelpLicenseTest.java
@@ -6,7 +6,13 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.Console;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jtestdb.aid.console.CcCapture;
 import org.testng.annotations.Test;
 
@@ -26,38 +32,95 @@ public class HelpLicenseTest {
 
     @Test
     public void HelpInteractive() {
-        CcCapture captureUnit = new CcCapture(System.out);
-        captureUnit.startCapture();
+//        CcCapture captureUnit = new CcCapture(System.out);
+//        captureUnit.startCapture();
 
         InputStream origSystemIn = System.in;
-        
-//         System.setIn(new BufferedInputStream());
 
-        String input = String.format("help\nh");
-        
-//        BufferedOutputStream out2InStream = new BufferedOutputStream(out);
-//
-//        ByteArrayOutputStream outToInStream = new ByteArrayOutputStream();
-//
-//       
-//
-        // run the main in its own thread so it starts
-        
+        String input = String.format("help\n");
+//        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        BufferedOutputStream outPipe = null;
+
+        PipedInputStream pipeIn = new PipedInputStream();
+        BufferedInputStream in = new BufferedInputStream(pipeIn);
+
+//         System.setIn(pipeIn);
+        PipedOutputStream ppp = new PipedOutputStream();
+
+        try {
+            ppp.connect(pipeIn);
+
+            ppp.write(input.getBytes());
+//            ppp.flush();
+//            ppp.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        System.setIn(in);
+
+      // run the main in its own thread so it starts
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                 Imatic8Prog.main(new String[]{});
+                Imatic8Prog.main(new String[]{});
             }
         };
         // in interactive mode main will wait on the input stream before
         // preceding to process, so the setIn below acts as an input
         Thread t = new Thread(r);
         t.start();
+        
+         try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(HelpLicenseTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+//        captureUnit.stopCapture();
+//        captureUnit.printAllLines();
 
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-
-        captureUnit.stopCapture();
-        captureUnit.printAllLines();
+        System.setIn(origSystemIn);
 
     }
+    
+    
+//    @Test (dependsOnMethods="HelpInteractive")
+//    public void HelpInteractive2() {
+////        CcCapture captureUnit = new CcCapture(System.out);
+////        captureUnit.startCapture();
+//
+//        InputStream origSystemIn = System.in;
+//        
+//        String input = String.format("help\n");
+//        System.setIn(new ByteArrayInputStream(input.getBytes()));
+//
+//        // run the main in its own thread so it starts  
+//        Runnable r = new Runnable() {
+//            @Override
+//            public void run() {
+//                 Imatic8Prog.main(new String[]{});
+//            }
+//        };
+//        // in interactive mode main will wait on the input stream before
+//        // preceding to process, so the setIn below acts as an input
+//        Thread t = new Thread(r);
+//        t.start();
+//        
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(HelpLicenseTest.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+////        String input = String.format("help");
+////        System.setIn(new ByteArrayInputStream(input.getBytes()));
+//   
+////        captureUnit.stopCapture();
+////        captureUnit.printAllLines();
+//        
+//        System.setIn(origSystemIn);
+//
+//    }
 }
